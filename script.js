dd30353d68553774b8f815437e53c23a20feb368
Mohit Chaudhary <110936710+MohitChaudhary26@users.noreply.github.com>
2025-03-16 19:45:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+{/* <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script src="https://kit.fontawesome.com/58a810656e.js" crossorigin="anonymous"></script> */}
+
+// var datatype = new Typed(".role",{
+//     string:[
+//         "Full Stack Developer", 
+//         "Web Developer",
+//         "Backend Developer ",
+//         "CODER",
+//     ], 
+//     loop:true,
+//     typeSpeed: 100,
+//     backSpeed: 80,
+//     backDelay: 1000,
+// });
+document.getElementById("userImage").addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+  
+
